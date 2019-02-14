4118f9414d34228a5757a6e9aff6f4fca9890e13
Mehtab Kayani <mehtab.kayani@gmail.com>
2019-02-14 15:05:03 +0100

    Worked on all the classes

---
+++ PizzaRest/src/java/PizzaDao/PizzaDao.java
@@ -0,0 +1,46 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package PizzaDao;
+
+import PizzaEntity.Pizza;
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+/**
+ *
+ * @author mehtab
+ */
+@Stateless
+public class PizzaDao {
+    
+    @PersistenceContext (name="PizzaRestPU")
+    EntityManager em;
+    
+    public void addPizza(Pizza p){
+        em.persist(p);
+    }
+    
+    public void editPizza(Pizza p){
+        em.merge(p);
+    }
+    
+    public void deletePizza (Integer id){
+        em.remove(em.find(Pizza.class, id));
+    }
+    
+    public List<Pizza> listAllPizza(){
+        return em.createQuery("Select p From Pizza p").getResultList();
+    }
+    
+    public Pizza findById(Integer id){
+       return em.find(Pizza.class, id);
+    }
+    
+    
+}
+
---
+++ PizzaRest/src/java/PizzaRestDao/ApplicationConfig.java
@@ -0,0 +1,13 @@
+package PizzaRestDao;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ *
+ * @author mehtab
+ */
+@ApplicationPath ("Webresources")
+public class ApplicationConfig extends Application{
+    
+}
